9a04a721fa62a4fb0cdf38108911858bcdc9e116
kabugh <kabugh00@gmail.com>
2023-03-17 10:37:59 +0100

    Remove JS event api project

--- JSEventAPIModelling/runDocker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -d local_mount ]; then
-	mkdir local_mount
-fi
-
-docker run --platform linux/amd64 --mount type=bind,source=`pwd`/local_mount,destination=/mount -w /home/playground -it api-modelling:latest
