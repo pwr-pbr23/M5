170bb962f505414dc84f750a312b89feec5b59e2
kabugh <kabugh00@gmail.com>
2023-06-16 12:01:43 +0200

    Add mcc measure

---
+++ comparisonModelsExperiment/script/measures.py
@@ -0,0 +1,90 @@
+import pandas as pd
+from math import sqrt
+
+deepLineDp_prediction_file = '../output/prediction/DeepLineDP/within-release/activemq-5.2.0.csv'
+rf_prediction_file = '../output/RF-line-level-result/activemq-5.2.0-line-lvl-result.csv'
+bow_prediction_file = '../output/prediction/BoW/within-release/activemq-5.2.0.csv'
+
+thresholds = [0.99, 0.9, 0.8, 0.6, 0.3, 0.1, 0.01, 0.0001, 0]
+
+def calculate_confusion_matrix_deepLine(df, threshold):
+    # True Positive (TP)
+    tp = len(df[(df['line-level-ground-truth'] == True) & (df['line-attention-score'] > threshold)])
+    
+    # True Negative (TN)
+    tn = len(df[(df['line-level-ground-truth'] == False) & (df['line-attention-score'] <= threshold)])
+
+    # False Positive (FP)
+    fp = len(df[(df['line-level-ground-truth'] == False) & (df['line-attention-score'] > threshold)])
+
+    # False Negative (FN)
+    fn = len(df[(df['line-level-ground-truth'] == True) & (df['line-attention-score'] <= threshold)])
+
+    return tp, tn, fp, fn
+
+def calculate_confusion_matrix_rf(df):
+    # True Positive (TP)
+    tp_df = df[(df['line-label'] == True) & (df['line-score-pred'] == 1)]
+    tp = 0 if tp_df.empty else len(tp_df)
+
+    # True Negative (TN)
+    tn_df = df[(df['line-label'] == False) & (df['line-score-pred'] == 0)]
+    tn = 0 if tn_df.empty else len(tn_df)
+
+    # False Positive (FP)
+    fp_df = df[(df['line-label'] == False) & (df['line-score-pred'] == 1)]
+    fp = 0 if fp_df.empty else len(fp_df)
+
+    # False Negative (FN)
+    fn_df = df[(df['line-label'] == True) & (df['line-score-pred'] == 0)]
+    fn = 0 if fn_df.empty else len(fn_df)
+
+    return tp, tn, fp, fn
+
+def calculate_confusion_matrix_bow(df):
+    # True Positive (TP)
+    tp_df = df[(df['file-level-ground-truth'] == True) & (df['prediction-label'] == True)]
+    tp = 0 if tp_df.empty else len(tp_df)
+
+    # True Negative (TN)
+    tn_df = df[(df['file-level-ground-truth'] == False) & (df['prediction-label'] == False)]
+    tn = 0 if tn_df.empty else len(tn_df)
+
+    # False Positive (FP)
+    fp_df = df[(df['file-level-ground-truth'] == False) & (df['prediction-label'] == True)]
+    fp = 0 if fp_df.empty else len(fp_df)
+
+    # False Negative (FN)
+    fn_df = df[(df['file-level-ground-truth'] == True) & (df['prediction-label'] == False)]
+    fn = 0 if fn_df.empty else len(fn_df)
+
+    return tp, tn, fp, fn
+
+def calculate_mcc(tp, tn, fp, fn):
+    numerator = (tp * tn) - (fp * fn)
+    denominator = sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
+    mcc = numerator / denominator if denominator != 0 else 0
+
+    return mcc
+
+df = pd.read_csv(rf_prediction_file)
+tp, tn, fp, fn = calculate_confusion_matrix_rf(df)
+mcc = calculate_mcc(tp, tn, fp, fn)
+
+print("Matthews Correlation Coefficient (MCC) RF:", mcc)
+
+df = pd.read_csv(bow_prediction_file)
+tp, tn, fp, fn = calculate_confusion_matrix_bow(df)
+mcc = calculate_mcc(tp, tn, fp, fn)
+
+print("Matthews Correlation Coefficient (MCC) BoW:", mcc)
+
+
+def get_dp_mcc_with_thresholds(df, thresholds):
+    for t in thresholds:
+        tp, tn, fp, fn = calculate_confusion_matrix_deepLine(df, t)
+        mcc = calculate_mcc(tp, tn, fp, fn)
+        print("Matthews Correlation Coefficient (MCC) DP :", mcc, "Threshold", t)
+
+df = pd.read_csv(deepLineDp_prediction_file)
+get_dp_mcc_with_thresholds(df, thresholds)
